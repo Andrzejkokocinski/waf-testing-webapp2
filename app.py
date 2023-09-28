82e4b82769b9455dd0ddd2b48989d90b02f559bc
Chris Polewiak <chris@polewiak.pl>
2023-09-28 23:51:27 +0200

    update app route

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/search')
-def search():
     args = request.args
     arg_search = args.get("search")
 
