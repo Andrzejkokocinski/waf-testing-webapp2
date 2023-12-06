7f930b9b86a15e2c22acbd056c177aa57354a1c1
Chris Polewiak <chris@polewiak.pl>
2023-12-07 00:21:32 +0100

    aaa

--- app.py
+++ app.py
@@ -2,27 +2,33 @@ import os
 from flask import Flask, request, render_template
 from azure.appconfiguration.provider import load, SettingSelector
 from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
 
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url="https://stlzone7682.blob.core.windows.net/", credential=DefaultAzureCredential() )
 
 ### Azure App Configuration ###
 from azure.appconfiguration import AzureAppConfigurationClient
 AppConfigUrl = "https://appcs-demo.azconfig.io"
+selects = SettingSelector(key_filter="testapp_settings_*")
+azure_app_config = ''
 azure_app_config = load(
                         endpoint=AppConfigUrl,
-                        keyvault_credential=credential,
-                        credential=credential
+                        credential=DefaultAzureCredential(),
+                        selects=[selects],
+                        trim_prefixes=["testapp_settings_"]
                    )
+app.config.update(azure_app_config)
 
 
 ### App Insights ###
-AzureAppInsights_ConnectionString=''
-AzureAppInsights_ConnectionString = azure_app_config.get_configuration_setting(key="appinsight")
+AzureAppInsights_ConnectionString = ''
+AzureAppInsights_ConnectionString = app.config.get("appinsight")
 if AzureAppInsights_ConnectionString:
     from opencensus.ext.azure.trace_exporter import AzureExporter
     from opencensus.ext.flask.flask_middleware import FlaskMiddleware
@@ -34,21 +40,43 @@ if AzureAppInsights_ConnectionString:
         sampler=ProbabilitySampler(rate=1.0),
     )
 
-### Test Config ###
-testconfig=''
-testconfig = AppConfigClient.get_configuration_setting(key="test")
-
 @app.route('/')
 def index():
+
+    context = {}
+
+    if (azure_app_config):
+        azure_app_config.refresh()
+        app.config.update(azure_app_config)
+
+    print('Request for index page received')
+
     args = request.args
     arg_search = args.get("search")
 
     get_request = {}
     if arg_search != '':
         get_request['search'] = arg_search
+        print(arg_search)
 
-    return render_template('index.html', request=get_request, azure_app_config="")
+    context['search'] = arg_search
+
+    return render_template('index.html', context=context)
 
 @app.route('/appconfig')
 def appconfig():
-    return render_template('index.html', azure_app_config=azure_app_config)
+
+    context = {}
+
+    if (azure_app_config):
+        azure_app_config.refresh()
+        app.config.update(azure_app_config)
+        context['token'] = app.config.get('token')
+
+    print('Request for appconfig page received')
+
+
+    return render_template('index.html', context=context)
+
+if __name__ == '__main__':
+   app.run()
