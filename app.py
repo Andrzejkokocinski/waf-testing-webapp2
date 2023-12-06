a7ae1c73eab493198a1b12e2639c067c2b2afdd1
Chris Polewiak <chris@polewiak.pl>
2023-12-06 22:58:07 +0100

    aaa

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import os
 import json
 from flask import (Flask, request, render_template)
 
-appinsight=''
-config_settings=''
-
 ### Managed Identity Credentials ###
 from azure.identity import DefaultAzureCredential
 credential = DefaultAzureCredential()
@@ -14,28 +11,27 @@ from azure.appconfiguration import AzureAppConfigurationClient
 AppConfigUrl = "https://appcs-demo.azconfig.io"
 AppConfigClient = AzureAppConfigurationClient(AppConfigUrl, credential)
 
-AppConfig_test = AppConfigClient.get_configuration_setting(key="test")
-
-
-
 ### APP ###
-
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-if appinsight:
+### App Insights ###
+AzureAppInsights_ConnectionString=''
+#AzureAppInsights_ConnectionString = AppConfigClient.get_configuration_setting(key="appinsight")
+if AzureAppInsights_ConnectionString:
     from opencensus.ext.azure.trace_exporter import AzureExporter
     from opencensus.ext.flask.flask_middleware import FlaskMiddleware
     from opencensus.trace.samplers import ProbabilitySampler
     # For website monitoring
-    AzureAppInsights_ConnectionString = 'InstrumentationKey=3712782d-691c-47d4-bef1-52ae3c0f7dc1;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/'
+    middleware = FlaskMiddleware(
+        app,
+        exporter=AzureExporter( connection_string=AzureAppInsights_ConnectionString ),
+        sampler=ProbabilitySampler(rate=1.0),
+    )
 
-    if AzureAppInsights_ConnectionString:
-        middleware = FlaskMiddleware(
-            app,
-            exporter=AzureExporter( connection_string=AzureAppInsights_ConnectionString ),
-            sampler=ProbabilitySampler(rate=1.0),
-        )
+### Test Config ###
+testconfig=''
+testconfig = AppConfigClient.get_configuration_setting(key="test")
 
 @app.route('/')
 def index():
@@ -46,8 +42,8 @@ def index():
     if arg_search != '':
         get_request['search'] = arg_search
 
-    return render_template('index.html', request=get_request, config_settings="")
+    return render_template('index.html', request=get_request, testconfig="")
 
 @app.route('/appconfig')
 def appconfig():
-    return render_template('index.html', config_settings=config_settings)
+    return render_template('index.html', testconfig=testconfig)
